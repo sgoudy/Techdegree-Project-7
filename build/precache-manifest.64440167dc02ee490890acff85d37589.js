bbf59a9d17cdbaf2531dac0198f5d03ee8f29083
Shelby Goudy <shelbygoudy@gmail.com>
2020-08-25 10:47:29 -0400

    register serviceworker

---
+++ build/precache-manifest.64440167dc02ee490890acff85d37589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac2d6e1dfc7304273b3472e0129c497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa5225bf116b9b5d0fa7",
+    "url": "/static/css/main.e4f988eb.chunk.css"
+  },
+  {
+    "revision": "3345782b971644abaa59",
+    "url": "/static/js/2.d0380a01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0380a01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5225bf116b9b5d0fa7",
+    "url": "/static/js/main.85eec91d.chunk.js"
+  },
+  {
+    "revision": "86437c04feae21a8df0e",
+    "url": "/static/js/runtime-main.44163c24.js"
+  }
+]);
